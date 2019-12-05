c16c8bf9426529a82d31499f9b353073a5a5de13
leonardhoward <lenhoward87@gmail.com>
2019-12-04 21:43:48 -0700

    fixed deployment issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
-import { Route, Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
